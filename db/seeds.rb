950e2de69023b8542a17c45744c3fc0ffa202431
mateo <balzamateo5@gmail.com>
2022-06-07 09:44:41 -0500

    Se comentaron los seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,18 +8,18 @@
 
 # universidades
 
-University.create(universidad: "CUC", pais: "Colombia")
-University.create(universidad: "Simon Bolivar", pais: "Colombia")
-University.create(universidad: "USC", pais: "Estados Unidos")
-University.create(universidad: "Duke", pais: "Estados Unidos")
-University.create(universidad: "University of Calgary", pais: "Canada")
-University.create(universidad: "University of Toronto", pais: "Canada")
-University.create(universidad: "UNAM", pais: "Mexico")
-University.create(universidad: "UNAL", pais: "Mexico")
-University.create(universidad: "Universidad de São Paulo", pais: "Brasil")
-University.create(universidad: "Universidad Federal de Río de Janeiro", pais: "Brasil")
-University.create(universidad: "Universidad Nacional de Trujillo", pais: "Peru")
-University.create(universidad: "Universidad Nacional Mayor de San Marcos", pais: "Peru")
+#University.create(universidad: "CUC", pais: "Colombia")
+#University.create(universidad: "Simon Bolivar", pais: "Colombia")
+#University.create(universidad: "USC", pais: "Estados Unidos")
+#University.create(universidad: "Duke", pais: "Estados Unidos")
+#University.create(universidad: "University of Calgary", pais: "Canada")
+#University.create(universidad: "University of Toronto", pais: "Canada")
+#University.create(universidad: "UNAM", pais: "Mexico")
+#University.create(universidad: "UNAL", pais: "Mexico")
+#University.create(universidad: "Universidad de São Paulo", pais: "Brasil")
+#University.create(universidad: "Universidad Federal de Río de Janeiro", pais: "Brasil")
+#University.create(universidad: "Universidad Nacional de Trujillo", pais: "Peru")
+#University.create(universidad: "Universidad Nacional Mayor de San Marcos", pais: "Peru")
 # University.create(universidad: "Universidad Nacional de Cajamarca", pais: "Peru")
 # University.create(universidad: "Universidad de Alcalá de Henares", pais: "España")
 # University.create(universidad: "Universidad Alfonso X", pais: "España")
@@ -38,50 +38,50 @@ University.create(universidad: "Universidad Nacional Mayor de San Marcos", pais:
 # University.create(universidad: "Universidad de palermo", pais: "Argentina")
 
 
-Program.create(nombre: "Ingeniería en Sistemas Computacionales",
-               beca: "Banco Santander, Ser pilo paga, Roble Amarillo, Gases del caribe",
-               requisito:"Presencial",
-               descripcion:"Ser colombiano, Sisben minimo vulnerable, Promedio de icfes superior a 250 puntos",
-               university_id: 1)
+#Program.create(nombre: "Ingeniería en Sistemas Computacionales",
+#               beca: "Banco Santander, Ser pilo paga, Roble Amarillo, Gases del caribe",
+#               requisito:"Presencial",
+#               descripcion:"Ser colombiano, Sisben minimo vulnerable, Promedio de icfes superior a 250 puntos",
+#               university_id: 1)
 
-Program.create(nombre: "Ingeniería en Industrial",
-               beca: "Banco Santander, Ser pilo paga, Roble Amarillo, Gases del caribe",
-               requisito:"Presencial",
-               descripcion:"Ser colombiano, Sisben minimo vulnerable, Promedio de icfes superior a 250 puntos",
-               university_id: 1)
+#Program.create(nombre: "Ingeniería en Industrial",
+#               beca: "Banco Santander, Ser pilo paga, Roble Amarillo, Gases del caribe",
+#               requisito:"Presencial",
+#               descripcion:"Ser colombiano, Sisben minimo vulnerable, Promedio de icfes superior a 250 puntos",
+#               university_id: 1)
 
-Program.create(nombre: "Medicina",
-               beca: "Banco Santander, Ser pilo paga, Roble Amarillo, Gases del caribe",
-               requisito:"Presencial",
-               descripcion:"Ser colombiano, Sisben minimo vulnerable, Promedio de icfes superior a 250 puntos",
-               university_id: 2)
+#Program.create(nombre: "Medicina",
+#               beca: "Banco Santander, Ser pilo paga, Roble Amarillo, Gases del caribe",
+#               requisito:"Presencial",
+#               descripcion:"Ser colombiano, Sisben minimo vulnerable, Promedio de icfes superior a 250 puntos",
+#               university_id: 2)
 
-Program.create(nombre: "Biomedica",
-               beca: "Banco Santander, Ser pilo paga, Roble Amarillo, Gases del caribe",
-               requisito:"Presencial",
-               descripcion:"Ser colombiano, Sisben minimo vulnerable, Promedio de icfes superior a 250 puntos",
-               university_id: 2)
+#Program.create(nombre: "Biomedica",
+#               beca: "Banco Santander, Ser pilo paga, Roble Amarillo, Gases del caribe",
+#               requisito:"Presencial",
+#               descripcion:"Ser colombiano, Sisben minimo vulnerable, Promedio de icfes superior a 250 puntos",
+#               university_id: 2)
                
-Program.create(nombre: "Fisica",
-               beca: " Becas Fulbright, AAUW International Fellowships, PEO International Peace, Becas Earl Warren",
-               requisito:"Presencial",
-               descripcion:" Hablar ingles C1, Promedio semestral minimo de 4, Tener buen comportamiento, Trabajo comunitario +40 horas semanales",
-               university_id: 3)
+#Program.create(nombre: "Fisica",
+#               beca: " Becas Fulbright, AAUW International Fellowships, PEO International Peace, Becas Earl Warren",
+#               requisito:"Presencial",
+#               descripcion:" Hablar ingles C1, Promedio semestral minimo de 4, Tener buen comportamiento, Trabajo comunitario +40 horas semanales",
+#               university_id: 3)
                
-Program.create(nombre: "Astronomia",
-               beca: " Becas Fulbright, AAUW International Fellowships, PEO International Peace, Becas Earl Warren",
-               requisito:"Presencial",
-               descripcion:" Hablar ingles C1, Promedio semestral minimo de 4, Tener buen comportamiento, Trabajo comunitario +40 horas semanales",
-               university_id: 3)
+#Program.create(nombre: "Astronomia",
+#               beca: " Becas Fulbright, AAUW International Fellowships, PEO International Peace, Becas Earl Warren",
+#               requisito:"Presencial",
+#               descripcion:" Hablar ingles C1, Promedio semestral minimo de 4, Tener buen comportamiento, Trabajo comunitario +40 horas semanales",
+#               university_id: 3)
 
-Program.create(nombre: "Fisica",
-               beca: "Vanier Canada Graduate Scholarships Calgary Graduate Studies Award Graduate Fellowships Sheridan College",
-               requisito:"Presencial",
-               descripcion:"Promedio semestral minimo de 4, Ser bachiller, Hablar ingles C1, Hablar frances basico, Tener buen comportamiento",
-               university_id: 4)
+#Program.create(nombre: "Fisica",
+#               beca: "Vanier Canada Graduate Scholarships Calgary Graduate Studies Award Graduate Fellowships Sheridan College",
+#               requisito:"Presencial",
+#               descripcion:"Promedio semestral minimo de 4, Ser bachiller, Hablar ingles C1, Hablar frances basico, Tener buen comportamiento",
+#               university_id: 4)
                
-Program.create(nombre: "Astronomia",
-               beca: " Vanier Canada Graduate Scholarships Calgary Graduate Studies Award Graduate Fellowships Sheridan College",
-               requisito:"Presencial",
-               descripcion:"Promedio semestral minimo de 4, Ser bachiller, Hablar ingles C1, Hablar frances basico, Tener buen comportamiento",
-               university_id: 4)
+#Program.create(nombre: "Astronomia",
+#               beca: " Vanier Canada Graduate Scholarships Calgary Graduate Studies Award Graduate Fellowships Sheridan College",
+#               requisito:"Presencial",
+#               descripcion:"Promedio semestral minimo de 4, Ser bachiller, Hablar ingles C1, Hablar frances basico, Tener buen comportamiento",
+#               university_id: 4)
